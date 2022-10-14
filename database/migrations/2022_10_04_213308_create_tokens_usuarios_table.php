88c670bce82eb8bf51badaafa140f3dcb57bb2c1
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-14 20:27:05 -0300

    Create 2022_10_04_213308_create_tokens_usuarios_table.php

---
+++ database/migrations/2022_10_04_213308_create_tokens_usuarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens_usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->string('token', 64)->unique();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens_usuarios');
+    }
+};

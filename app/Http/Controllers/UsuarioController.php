0ae9886b93efc9e2d743aa65957755dc5d33f540
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-15 12:53:31 -0300

    Update UsuarioController.php

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TipoDocumento;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
     //
+
+    public function home(Request $request)
+    {
+        $tiposDocumentos = TipoDocumento::all('descripcion')->toJson();
+        $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos \nDocumento: \n";
+        return response($msj, 200)
+            ->header('Content-Type', 'text/plain');
+    }
 }

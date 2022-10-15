9ef08615b1e31e6e186e33ef5ad3b1acb32394da
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-15 16:59:44 -0300

    controller

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Persona;
 use App\Models\TipoDocumento;
 use App\Models\Usuario;
 use Illuminate\Http\Request;
@@ -10,11 +11,20 @@ class UsuarioController extends Controller
 {
     //
 
-    public function home(Request $request)
+    public function home()
     {
-        $tiposDocumentos = Usuario::all();
-        $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento:  \nDocumento: \n";
+        $tiposDocumentos = json_encode(['DNI', 'LC', 'LE', 'CI']);
+        $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n";
         return response($msj, 200)
+            ->header('Content-Type', 'text/plain');
+    }
+
+    public function login(Request $request)
+    {
+        $tiposDocumentos = Persona::whereId(1)->first()->grupoSanguineo;
+
+        // $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n";
+        return response($tiposDocumentos, 200)
             ->header('Content-Type', 'text/plain');
     }
 }

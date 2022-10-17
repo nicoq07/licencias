30728f8eaaf15e4db52194310580940f0208279b
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-17 13:15:05 -0300

    models

--- app/Models/ExamenPregunta.php
+++ app/Models/ExamenPregunta.php
@@ -5,6 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\ExamenPregunta
+ *
+ * @property int $id
+ * @property int $pregunta_id
+ * @property int $examen_id
+ * @property int $resultado_al_responder
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\ExamenPreguntaFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta whereExamenId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta wherePreguntaId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta whereResultadoAlResponder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExamenPregunta whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class ExamenPregunta extends Model
 {
     use HasFactory;
--- app/Models/PreguntaExamen.php
+++ app/Models/PreguntaExamen.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\PreguntaExamen
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|PreguntaExamen newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PreguntaExamen newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PreguntaExamen query()
+ * @mixin \Eloquent
+ */
 class PreguntaExamen extends Model
 {
     use HasFactory;

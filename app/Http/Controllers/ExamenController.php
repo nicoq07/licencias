45c62b269d8ca1a3ec76d0a9b35d18f53a1ceab6
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-17 14:46:49 -0300

    Update ExamenController.php

--- app/Http/Controllers/ExamenController.php
+++ app/Http/Controllers/ExamenController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UsuarioService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Validator;
@@ -10,14 +11,13 @@ class ExamenController extends Controller
 {
     //
 
-    public function cuestionarioInicial(Request $request)
+    public function cuestionarioInicial(Request $request, UsuarioService $usuarioService)
     {
         $validator = Validator::make($request->all(), [
             'tipo_documento' => 'bail|required',
             'documento' => 'required|numeric',
             'utiliza_anteojos' => 'required'
         ]);
-
         if ($validator->fails()) {
             return response(
                 [
@@ -26,6 +26,14 @@ class ExamenController extends Controller
                 ]
             );
         }
+
+        //ver si utiliza anteojos y actualizar en la clasePersona
+        //Si utiliza generar un turno para una revision
+
+        //Si no utiliza, avanzar con el cuestionario
+
+
+
         return response('ok', 200);
     }
 }

a74e57858bbaa58abe313f277d3c08184a39e40c
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-17 10:28:56 -0300

    Update UsuarioController.php

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,6 +7,8 @@ use App\Models\Persona;
 use App\Models\TipoDocumento;
 use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Validator;
 
 class UsuarioController extends Controller
 {
@@ -21,9 +23,22 @@ class UsuarioController extends Controller
 
     public function login(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'tipo_documento' => 'required',
+            'documento' => 'required|numeric'
+        ]);
+
+        if ($validator->fails()) {
+            return response(
+                [
+                    'errors' => $validator->errors(),
+                    'status' => Response::HTTP_BAD_REQUEST
+                ]
+            );
+        }
         // $tiposDocumentos = Persona::whereId(1)->first()->grupoSanguineo;
 
-        $tiposDocumentos = Examen::whereId(1)->first()->preguntas()->first();
+        $tiposDocumentos = Examen::whereId(1)->first()->preguntas()->first()->respuesta;
 
         // $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n";
         return response($tiposDocumentos, 200);

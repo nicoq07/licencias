6e5194b42f7920ef7c3c6a38ef7ac4c0be92a43e
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-16 21:53:33 -0300

    Update UsuarioController.php

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Examen;
 use App\Models\Persona;
 use App\Models\TipoDocumento;
 use App\Models\Usuario;
@@ -15,16 +16,16 @@ class UsuarioController extends Controller
     {
         $tiposDocumentos = json_encode(['DNI', 'LC', 'LE', 'CI']);
         $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n";
-        return response($msj, 200)
-            ->header('Content-Type', 'text/plain');
+        return response($msj, 200);
     }
 
     public function login(Request $request)
     {
-        $tiposDocumentos = Persona::whereId(1)->first()->grupoSanguineo;
+        // $tiposDocumentos = Persona::whereId(1)->first()->grupoSanguineo;
+
+        $tiposDocumentos = Examen::whereId(1)->first()->preguntas()->first();
 
         // $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n";
-        return response($tiposDocumentos, 200)
-            ->header('Content-Type', 'text/plain');
+        return response($tiposDocumentos, 200);
     }
 }

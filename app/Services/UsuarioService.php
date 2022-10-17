f4f4a6c6d0183b10dabbe41eaf01b2dd9562d18b
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-17 14:46:46 -0300

    Update UsuarioService.php

--- app/Services/UsuarioService.php
+++ app/Services/UsuarioService.php
@@ -1,3 +1,30 @@
 <?php
 
 namespace App\Services;
+
+use App\Models\TokenUsuario;
+use App\Models\Usuario;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+class UsuarioService
+{
+
+
+    public function generarTokenUsuario($usuario_id)
+    {
+        $tokenUsuario = new TokenUsuario();
+        $tokenUsuario->usuario_id = $usuario_id;
+        $tokenUsuario->token = Str::random(32);
+        $tokenUsuario->expires_at = Carbon::now()->add(1, 'hour')->toDateTimeString();
+
+        $tokenUsuario->save();
+
+        return $tokenUsuario->token;
+    }
+
+    public function utilizaAnteojos($data)
+    {
+        //
+    }
+}

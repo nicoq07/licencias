aaed418e8ccd754e87d113a20e91cc829ff55dc2
Nicolás Quiroga <nicoq07@gmail.com>
2022-10-17 13:15:00 -0300

    controllers

--- app/Http/Controllers/ExamenController.php
+++ app/Http/Controllers/ExamenController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Validator;
 
 class ExamenController extends Controller
 {
     //
+
+    public function cuestionarioInicial(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'tipo_documento' => 'bail|required',
+            'documento' => 'required|numeric',
+            'utiliza_anteojos' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response(
+                [
+                    'errors' => $validator->errors(),
+                    'status' => Response::HTTP_BAD_REQUEST
+                ]
+            );
+        }
+        return response('ok', 200);
+    }
 }
--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,6 +7,7 @@ use App\Models\Persona;
 use App\Models\TipoDocumento;
 use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use Validator;
 
@@ -17,30 +18,32 @@ class UsuarioController extends Controller
     public function home()
     {
         $tiposDocumentos = json_encode(['DNI', 'LC', 'LE', 'CI']);
-        $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n";
+        $msj = "Bienvenido al Sistema de Licencias,\n por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n Utiliza Anteojos:...";
         return response($msj, 200);
     }
 
-    public function login(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'tipo_documento' => 'required',
-            'documento' => 'required|numeric'
-        ]);
+    // public function doLogin(Request $request)
+    // {
 
-        if ($validator->fails()) {
-            return response(
-                [
-                    'errors' => $validator->errors(),
-                    'status' => Response::HTTP_BAD_REQUEST
-                ]
-            );
-        }
-        // $tiposDocumentos = Persona::whereId(1)->first()->grupoSanguineo;
+    //     if (Auth::attempt(request()->only('documento', 'tipo_documento'))) {
+    //         return response([
+    //             'AuthUser' => Auth::user()->tipo_documento
+    //         ], 200);
+    //     }
 
-        $tiposDocumentos = Examen::whereId(1)->first()->preguntas()->first()->respuesta;
+    //     return back()->withErrors([
+    //         'documento' => 'invalid credentials',
+    //     ]);
+    // }
 
-        // $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n";
-        return response($tiposDocumentos, 200);
-    }
+    // public function test()
+    // {
+
+    //     // $tiposDocumentos = Persona::whereId(1)->first()->grupoSanguineo;
+
+    //     $tiposDocumentos = Examen::whereId(1)->first()->preguntas()->first()->respuesta;
+
+    //     // $msj = "Bienvenido al Sistema de Licencias, por favor ingrese su \nTipo de Documento: $tiposDocumentos  \nDocumento: ... \n";
+    //     return response($tiposDocumentos, 200);
+    // }
 }
